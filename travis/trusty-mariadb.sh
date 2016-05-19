8c3c1788858c69f9b448579736f2f1f5ed2a794f
JoungKyun Kim <joungkyun@gmail.com>
2016-05-20 00:15:22 +0900

    update

--- travis/trusty-mariadb.sh
+++ travis/trusty-mariadb.sh
@@ -14,6 +14,6 @@ apt-get install software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 add-apt-repository "deb [arch=amd64,i386] http://ftp.kaist.ac.kr/mariadb/repo/${VER}/ubuntu trusty main"
 apt-get update
-apt-get install mariadb-server
+apt-get install -y mariadb-server
 
 exit 0
