ad8ffdc276b6f489b1acf06aac604164ad177bd9
JoungKyun Kim <joungkyun@gmail.com>
2016-05-20 18:15:38 +0900

    travis last test

--- travis/test.sh
+++ travis/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+res=0
+echo
+
 # add udf
 
 if [ -f "../docs/aes256_install.sql" ]; then
@@ -10,6 +13,45 @@ fi
 
 [ $? -ne 0 ] && echo "Failed registration UDF" > /dev/stdout && exit 1
 
+
+# AES-128 test
+
+KEY="0123456789abcdef"
+
+enc=$(mysql -u root -e "select HEX(AES256_ENCRYPT(\"asdf\", \"${KEY}\"))" | tail -n 1)
+enc1=$(mysql -u root -e "select HEX(AES_ENCRYPT(\"asdf\", \"${KEY}\"))" | tail -n 1)
+
+if [ "${enc}" = "${enc1}" ]; then
+	dec=$(mysql -u root -e "select AES256_DECRYPT(UNHEX(\"${enc}\"), \"${KEY}\")" | tail -n 1)
+
+	if [ "${dec}" = "asdf" ]; then
+		echo "Pass: aes192 encrypt and decrypt"
+	else
+		echo "Failed: aes192 encrypt and decrypt"
+		res=1
+	fi
+else
+	echo "Failed: aes128 encrypt"
+	res=1
+fi
+
+
+# AES-192 test
+
+KEY="0123456789abcdef01234567"
+
+enc=$(mysql -u root -e "select HEX(AES256_ENCRYPT(\"asdf\", \"${KEY}\"))" | tail -n 1)
+dec=$(mysql -u root -e "select AES256_DECRYPT(UNHEX(\"${enc}\"), \"${KEY}\")" | tail -n 1)
+
+if [ "${dec}" = "asdf" ]; then
+	echo "Pass: aes192 encrypt and decrypt"
+else
+	echo "Failed: aes192 encrypt and decrypt"
+	res=1
+fi
+
+# AES-256 test
+
 KEY="0123456789abcdef0123456789abcdef"
 
 enc=$(mysql -u root -e "select HEX(AES256_ENCRYPT(\"asdf\", \"${KEY}\"))" | tail -n 1)
@@ -19,7 +61,7 @@ if [ "${dec}" = "asdf" ]; then
 	echo "Pass: aes256 encrypt and decrypt"
 else
 	echo "Failed: aes256 encrypt and decrypt"
-	exit 1
+	res=1
 fi
 
-exit 0
+exit $res
