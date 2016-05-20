90485bfc7efa03a2cdcfb74d1ab75b8fd8e90aff
JoungKyun Kim <joungkyun@gmail.com>
2016-05-20 14:54:45 +0900

    10.0 test

--- travis/trusty-mariadb.sh
+++ travis/trusty-mariadb.sh
@@ -2,18 +2,22 @@
 
 VER=$1
 
+MIRROR="sfo1.mirrors.digitalocean.com"
+#MIRROR="ftp.kaist.ac.kr"
+
 case "$VER" in
-	'5.5' | '10.1' | '10.2')
+	'5.5' )
+		;;
+	'10.1' | '10.2')
+		apt-get -y install software-properties-common
+		apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+		add-apt-repository "deb [arch=amd64,i386] http://${MIRROR}/mariadb/repo/${VER}/ubuntu trusty main"
 		;;
 	*)
 		echo "Unsupport version \"$VER\"" > /dev/stdout
 		exit 1
 esac
 
-#apt-get -y install software-properties-common
-#apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-#add-apt-repository "deb [arch=amd64,i386] http://sfo1.mirrors.digitalocean.com/mariadb/repo/${VER}/ubuntu trusty main"
-#add-apt-repository "deb [arch=amd64,i386] http://ftp.kaist.ac.kr/mariadb/repo/${VER}/ubuntu trusty main"
 apt-get update
 apt-get install -y mariadb-server
 
