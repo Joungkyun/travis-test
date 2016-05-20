f1a269e33f728d71e23165192530291a64a45a75
JoungKyun Kim <joungkyun@gmail.com>
2016-05-20 14:57:13 +0900

    10.0 test 2

--- travis/trusty-mariadb.sh
+++ travis/trusty-mariadb.sh
@@ -8,7 +8,7 @@ MIRROR="sfo1.mirrors.digitalocean.com"
 case "$VER" in
 	'5.5' )
 		;;
-	'10.1' | '10.2')
+	'10.0' | '10.1' | '10.2')
 		apt-get -y install software-properties-common
 		apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 		add-apt-repository "deb [arch=amd64,i386] http://${MIRROR}/mariadb/repo/${VER}/ubuntu trusty main"
