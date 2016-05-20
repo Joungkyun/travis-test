b63195c6d9990919a200396fab7da77c46579d59
JoungKyun Kim <joungkyun@gmail.com>
2016-05-20 17:07:13 +0900

    10.0 test 5

--- travis/trusty-mariadb.sh
+++ travis/trusty-mariadb.sh
@@ -7,11 +7,14 @@ MIRROR="sfo1.mirrors.digitalocean.com"
 
 case "$VER" in
 	'5.5' )
+		INSTPKG="mariadb-server"
 		;;
 	'10.0' | '10.1' | '10.2')
 		apt-get -y install software-properties-common
 		apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 		add-apt-repository "deb [arch=amd64,i386] http://${MIRROR}/mariadb/repo/${VER}/ubuntu trusty main"
+
+		INSTPKG="mariadb-server libmariadbclient-dev"
 		;;
 	*)
 		echo "Unsupport version \"$VER\"" > /dev/stdout
@@ -19,6 +22,6 @@ case "$VER" in
 esac
 
 apt-get update
-apt-get install -y mariadb-server
+apt-get install -y ${INSTPKG}
 
 exit 0
